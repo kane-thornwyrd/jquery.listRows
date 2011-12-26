9d4c3979bcb4d202113714c2fe7a8ec674073e4e
Jean-Cédric THÉROND <jean.cedric.t@gmail.com>
2011-12-26 16:05:13 +0100

    tentative de rewrite en Coffeescript, à évaluer…

---
+++ jquery.listRows.coffee
@@ -0,0 +1,33 @@
+``
+((global, $, undef)->
+  throw 'jQuery is missing' if $ not instanceof jQuery
+
+  $.fn.listRaws = (iNumRaws)->
+    $selection = $ []
+    @.each ()->
+      $selection = $selection.add this if not $(this).is 'ul, ol'
+
+      $lists = $ this
+      $children = $lists.children()
+      id = $lists.attr 'id'
+      thisNumRaws = iNumRaws || (this.className.match /raws(\d+)/ || [])[1] || 5
+      iNumCols = Math.ceil $children.length / thisNumRaws
+
+      if iNumCols < 2
+        $selection = $selection.add this
+
+      $children.remove()
+
+      for elements, key in numCols when key < iNumCols
+        $lists = $lists.add $(this).clone().each(()->
+            $(this).addClass('last') if key is iNumCols - 1
+            this.id = id + '-' + key if id
+          ).insertAfter($lists[key-1])
+
+      for indice in [0..iNumCols]
+        sliceStart = indice*thisNumRaws
+        $children.slice(sliceStart, sliceStart + thisNumRaws).appendTo $lists[indice]
+
+    $selection = $selection.add $lists
+
+)(window, jQuery)
--- jquery.listRows.js
+++ jquery.listRows.js
@@ -1,58 +1,37 @@
-(function(window, $, undef) {
-	if($ === undef) {
-		throw 'Dépendence non satisfaite : jQuery';
-	}
 
-	$.fn.listRaws = function(iNumRaws) {
-		var $selection = $([]);
-		this.each(function() {
-			if(!$(this).is('ul, ol')) {
-				$selection = $selection.add(this);
-				return;
-			}
+;
 
-			var i
-			,sliceStart
-			,$lists = $(this)
-			,$children = $lists.children()
-			,id = $lists.attr('id')
-			,thisNumRaws = iNumRaws || (this.className.match(/raws(\d+)/) || [])[1] || 5
-			,iNumCols = Math.ceil($children.length / thisNumRaws);
-
-			if(iNumCols < 2) {
-				$selection = $selection.add(this);
-				return;
-			}
-
-			$children.remove();
-
-			for(i = 1; i < iNumCols; i++) {
-				$lists = $lists
-					.add(
-						$(this)
-							.clone()
-							.each(function() {
-								if(i === iNumCols - 1) {
-									$(this).addClass('last');
-								}
-								if(id) {
-									this.id = id + '-' + i;
-								}
-							})
-							.insertAfter($lists[i-1])
-					);
-			}
-
-			for(i = 0; i < iNumCols; i++) {
-				sliceStart = i*thisNumRaws;
-
-				$children
-					.slice(sliceStart, sliceStart + thisNumRaws)
-						.appendTo($lists[i]);
-			}
-
-			$selection = $selection.add($lists);
-		});
-		return $selection;
-	};
-})(this, this.jQuery);
+(function(global, $, undef) {
+  if (!($ instanceof jQuery)) throw 'jQuery is missing';
+  return $.fn.listRaws = function(iNumRaws) {
+    var $selection;
+    $selection = $([]);
+    this.each(function() {
+      var $children, $lists, elements, iNumCols, id, indice, key, sliceStart, thisNumRaws, _len, _results;
+      if (!$(this).is('ul, ol')) $selection = $selection.add(this);
+      $lists = $(this);
+      $children = $lists.children();
+      id = $lists.attr('id');
+      thisNumRaws = iNumRaws || (this.className.match(/raws(\d+)/ || []))[1] || 5;
+      iNumCols = Math.ceil($children.length / thisNumRaws);
+      if (iNumCols < 2) $selection = $selection.add(this);
+      $children.remove();
+      for (key = 0, _len = numCols.length; key < _len; key++) {
+        elements = numCols[key];
+        if (key < iNumCols) {
+          $lists = $lists.add($(this).clone().each(function() {
+            if (key === iNumCols - 1) $(this).addClass('last');
+            if (id) return this.id = id + '-' + key;
+          }).insertAfter($lists[key - 1]));
+        }
+      }
+      _results = [];
+      for (indice = 0; 0 <= iNumCols ? indice <= iNumCols : indice >= iNumCols; 0 <= iNumCols ? indice++ : indice--) {
+        sliceStart = indice * thisNumRaws;
+        _results.push($children.slice(sliceStart, sliceStart + thisNumRaws).appendTo($lists[indice]));
+      }
+      return _results;
+    });
+    return $selection = $selection.add($lists);
+  };
+})(window, jQuery);
